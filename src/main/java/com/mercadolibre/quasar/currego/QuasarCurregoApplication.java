08b136bf516515d9d6c78e2b29fd426dd0585a5d
curregobvc <camilo.urrego@bvc.com.co>
2023-02-01 17:55:12 -0500

    App created

---
+++ src/main/java/com/mercadolibre/quasar/currego/QuasarCurregoApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.quasar.currego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuasarCurregoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuasarCurregoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercadolibre/quasar/currego/QuasarCurregoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.mercadolibre.quasar.currego;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuasarCurregoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+
+}
