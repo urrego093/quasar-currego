89950b5794d27c28ad6f381131c48a5a6c0a7979
urrego093 <urrego@outlook.com>
2023-02-01 18:17:16 -0500

    dummy int

--- src/main/java/com/mercadolibre/quasar/currego/QuasarCurregoApplication.java
+++ src/main/java/com/mercadolibre/quasar/currego/QuasarCurregoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuasarCurregoApplication {
 
 	public static void main(String[] args) {
+		int a = 0;
 		SpringApplication.run(QuasarCurregoApplication.class, args);
 	}
 
