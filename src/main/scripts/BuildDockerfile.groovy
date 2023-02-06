46549c7493bb5c03cbf5219231ab19cc80178e1c
urrego093 <urrego@outlook.com>
2023-02-06 17:49:07 -0500

    Fixed minor bug while packaging the app the app should be packaged with mvn clean install docker:build or run with mvn clean install docker:run
    https://dmp.fabric8.io/
    section 5

--- src/main/scripts/BuildDockerfile.groovy
+++ src/main/scripts/BuildDockerfile.groovy
@@ -3,10 +3,10 @@ String template = new File("${project.basedir}/src/main/docker/DockerfileTemplat
 def dockerFileText = new groovy.text.SimpleTemplateEngine().createTemplate(template).make([fileName: project.build.finalName])
 
 println "writing dir " + "${project.basedir}/target/dockerfile"
-new File("${project.basedir}/target/".toString()).mkdirs()
+new File("${project.basedir}/target/dockerfile/".toString()).mkdirs()
 
 println "writing file"
-File dockerFile = new File("${project.basedir}/target/Dockerfile".toString())
+File dockerFile = new File("${project.basedir}/target/dockerfile/Dockerfile".toString())
 
 dockerFile.withWriter('UTF-8') { writer ->
     writer.write(dockerFileText)
